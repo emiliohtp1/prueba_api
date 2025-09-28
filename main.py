e4606e3ef8c83f61133655f40a0d77f9eff53b37
emilio.htp <emilio.htp@outlook.com>
2025-09-28 16:38:46 -0600

    a

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
+import os
+from pymongo import MongoClient
 from fastapi import FastAPI
 from pydantic import BaseModel
-from pymongo import MongoClient
-from bson.objectid import ObjectId
 from typing import List
-import os
 
-db_password = "PUyvTLcwWKOQ4wwM"
-mongo_link = f"mongodb+srv://emiliohtp_db_user:{db_password}@cluster0.cvdcchr.mongodb.net/"
-
-# Conexión a MongoDB (desde variable de entorno)
-MONGO_URI = os.getenv(mongo_link)
+# URI desde variable de entorno
+MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["prueba"]   # base de datos
 collection = db["prueba_collection"]
 
-# App FastAPI
 app = FastAPI()
 
-# Modelo para la entrada de datos
+# Modelos
 class Item(BaseModel):
     name: str
     description: str
@@ -25,6 +20,7 @@ class Item(BaseModel):
 class ProductosInput(BaseModel):
     productos: List[str]
 
+# Endpoints
 @app.get("/")
 def root():
     return {"msg": "API FastAPI + MongoDB funcionando en Render"}
@@ -36,31 +32,17 @@ def create_item(item: Item):
 
 @app.get("/items")
 def get_items():
-    items = []
-    for doc in collection.find():
-        items.append({
-            "id": str(doc["_id"]),
-            "name": doc["name"],
-            "description": doc["description"]
-        })
+    items = [{"id": str(doc["_id"]), "name": doc["name"], "description": doc["description"]} 
+             for doc in collection.find()]
     return items
 
 @app.post("/productos")
 def create_productos(data: ProductosInput):
-    # Convertir lista en documentos para MongoDB
     docs = [{"nombre": producto} for producto in data.productos]
     result = collection.insert_many(docs)
-    return {
-        "inserted_ids": [str(_id) for _id in result.inserted_ids],
-        "msg": "Productos agregados correctamente"
-    }
+    return {"inserted_ids": [str(_id) for _id in result.inserted_ids], "msg": "Productos agregados correctamente"}
 
 @app.get("/productos")
 def get_productos():
-    productos = []
-    for doc in collection.find():
-        productos.append({
-            "id": str(doc["_id"]),
-            "nombre": doc["nombre"]
-        })
+    productos = [{"id": str(doc["_id"]), "nombre": doc["nombre"]} for doc in collection.find()]
     return productos
