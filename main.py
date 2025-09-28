9f4ab0528915ee40a9647882b7e76bd3ef2273b8
emilio.htp <emilio.htp@outlook.com>
2025-09-28 16:49:06 -0600

    Conexion directa a MongoDB Atlas sin variables de entorno

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ database = "prueba"
 
 MONGO_URI = f"mongodb+srv://{db_user}:{db_password}@{cluster}/{database}?retryWrites=true&w=majority"
 client = MongoClient(MONGO_URI)
-db = client[database]   # base de datos
+db = client[database]
 collection = db["prueba_collection"]
 
 app = FastAPI()
@@ -29,24 +29,8 @@ class ProductosInput(BaseModel):
 def root():
     return {"msg": "API FastAPI + MongoDB funcionando en Render"}
 
-@app.post("/items")
-def create_item(item: Item):
-    result = collection.insert_one(item.dict())
-    return {"id": str(result.inserted_id), "msg": "Item creado"}
-
-@app.get("/items")
-def get_items():
-    items = [{"id": str(doc["_id"]), "name": doc["name"], "description": doc["description"]} 
-             for doc in collection.find()]
-    return items
-
 @app.post("/productos")
 def create_productos(data: ProductosInput):
     docs = [{"nombre": producto} for producto in data.productos]
     result = collection.insert_many(docs)
     return {"inserted_ids": [str(_id) for _id in result.inserted_ids], "msg": "Productos agregados correctamente"}
-
-@app.get("/productos")
-def get_productos():
-    productos = [{"id": str(doc["_id"]), "nombre": doc["nombre"]} for doc in collection.find()]
-    return productos
