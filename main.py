3b53ba6870decfddfc331819cad9eff47634af9c
emilio.htp <emilio.htp@outlook.com>
2025-09-28 16:56:26 -0600

    os.getenv(mongo_link)

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
-from pymongo import MongoClient
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pymongo import MongoClient
 from typing import List
 
-# Conexión directa a MongoDB Atlas
-db_user = "emiliohtp_db_user"
-db_password = "PUyvTLcwWKOQ4wwM"
-cluster = "cluster0.cvdcchr.mongodb.net"
-database = "prueba"
+# Tu URI completo de MongoDB Atlas
+MONGO_URI = "mongodb+srv://emiliohtp_db_user:PUyvTLcwWKOQ4wwM@cluster0.cvdcchr.mongodb.net/?retryWrites=true&w=majority"
 
-MONGO_URI = f"mongodb+srv://{db_user}:{db_password}@{cluster}/{database}?retryWrites=true&w=majority"
 client = MongoClient(MONGO_URI)
-db = client[database]
+db = client["prueba"]
 collection = db["prueba_collection"]
 
 app = FastAPI()
 
-# Modelos
-class Item(BaseModel):
-    name: str
-    description: str
-
 class ProductosInput(BaseModel):
     productos: List[str]
-
-# Endpoints
-@app.get("/")
-def root():
-    return {"msg": "API FastAPI + MongoDB funcionando en Render"}
 
 @app.post("/productos")
 def create_productos(data: ProductosInput):
     docs = [{"nombre": producto} for producto in data.productos]
     result = collection.insert_many(docs)
     return {"inserted_ids": [str(_id) for _id in result.inserted_ids], "msg": "Productos agregados correctamente"}
+
+@app.get("/productos")
+def get_productos():
+    productos = [{"id": str(doc["_id"]), "nombre": doc["nombre"]} for doc in collection.find()]
+    return productos
